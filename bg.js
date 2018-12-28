4c69f11703061db695901b99bdbd5ba1476aa966
yunashin <yuna4573@naver.com>
2018-12-29 01:15:41 +0900

    Backgound init

---
+++ bg.js
@@ -0,0 +1,22 @@
+const body = document.querySelector('body');
+
+const imgNum = 5;
+
+function paintImage(imgNum) {
+    const image = new Image();
+    image.src = `images/${imgNum + 1}.jpg`;
+    image.classList.add('bg_img');
+    body.prepend(image);
+}
+
+function genRandom() {
+    const num = Math.floor(Math.random() * imgNum);
+    return num;
+}
+
+function init() {
+    const randomNum = genRandom();
+    paintImage(randomNum);
+}
+
+init();
